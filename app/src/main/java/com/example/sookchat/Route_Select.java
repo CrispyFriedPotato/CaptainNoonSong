62237a317baf973c1086b39ea4885ca07dae663b
JihoLee <jiho960214@naver.com>
2019-08-21 14:05:14 +0900

    Finished organization 1. tour_select map(Almost) 2.route_select

--- app/src/main/java/com/example/sookchat/Route_Select.java
+++ app/src/main/java/com/example/sookchat/Route_Select.java
@@ -6,7 +6,13 @@ import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -15,27 +21,23 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.sookchat.Main.MainActivity;
+
 import static android.content.Context.LOCATION_SERVICE;
 
 
-public class Route_Select extends Fragment {
-    private Spinner spinner1;
-    private Spinner spinner2;
-    private static final String[] schools = new String[]{"새힘관", "명신관", "진리관","순헌관","학생회관","행정관","수련관","행파관","이과대학","중앙 도서회관","사회 교육관","미술 대학","약학 대학","백주년 기념관","음악 대학"};
-    public Route_Select() {
+public class Route_Select extends Fragment{
+     public Route_Select() {
         // Required empty public constructor
     }
-
-    public static Route_Select newInstance(String param1, String param2) {
-        Route_Select fragment = new Route_Select();
-
-        return fragment;
+    public static Route_Select newInstance(){
+        return new Route_Select();
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       }
+
+    }
 
     private void gpsCheck() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -63,58 +65,39 @@ public class Route_Select extends Fragment {
         startActivity(gpsOptionsIntent);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         LocationManager locationManager = (LocationManager)getActivity().getSystemService(LOCATION_SERVICE);
         if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             gpsCheck();
         }
-
-
+        //naver api 불러오기
+        FragmentManager fm = getChildFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.route_map);
         View view = inflater.inflate(R.layout.fragment_route_select,container,false);
 
-        spinner1= (Spinner)view.findViewById(R.id.spinner_start);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_spinner_item,schools);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter1);
-        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+        //toolbar
+        Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        toolbar.findViewById(R.id.toolbar_title).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ((MainActivity)getActivity()).replaceFragment(4);
             }
         });
-
-        spinner2 = (Spinner)view.findViewById(R.id.spinner_destination);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_spinner_item,schools);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //spinner2.setPrompt("도착 장소 선택");
-        spinner2.setAdapter(adapter2);
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-        //spinner.setOnItemSelectedListener(getActivity());
         // Inflate the layout for this fragment
         return view;
     }
-
-    }
+}
 
 
 
--- app/src/main/java/com/example/sookchat/Tour_View.java
+++ app/src/main/java/com/example/sookchat/Tour_View.java
@@ -1,20 +1,29 @@
 package com.example.sookchat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.util.FusedLocationSource;
 import com.naver.maps.map.widget.LocationButtonView;
 
 
-public class Tour_View extends Activity {
+public class Tour_View extends Activity implements OnMapReadyCallback {
     // NaverMap API 3.0
     private MapView mapView;
     private LocationButtonView locationButtonView;
@@ -38,19 +47,62 @@ public class Tour_View extends Activity {
         tourId = intent.getIntExtra("tourId", 0);
         Log.e(TAG, "tourId = " + tourId);
 
-
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
 
+
+    }
+    private void gpsCheck() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("계속하려면 위치 기능 설정이 필요합니다.")
+                .setCancelable(false)
+                .setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                turnOnGps();
+                            }
+                        })
+                .setNegativeButton("아니요",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    // show GPS Options
+    private void turnOnGps() {
+        Intent gpsOptionsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(gpsOptionsIntent);
     }
 
     public void onMapReady(@NonNull final NaverMap naverMap) {
+        //map을 생성할 준비가 되었을때 가장 먼저 호출되는 오버라이드 메소드
+        // 지도에 관한 설정(초기화)은 모두 onMapReady에서
+
         //naverMap.getUiSettings().setLocationButtonEnabled(true);
-        locationButtonView.setMap(naverMap);
+        //locationButtonView.setMap(naverMap);
 
         // Location Change Listener을 사용하기 위한 FusedLocationSource 설정
         naverMap.setLocationSource(locationSource);
         naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
+        naverMap.setBuildingHeight(0.5f);
+        naverMap.setIndoorEnabled(true);
+
+        //여기서 사용자가 어느 캠퍼스를 선택했냐에 따라 보여주는 카메라 포지션과 서북단 동남단 제한이 달라지면 될듯요.
+        //**********2019.08.21 근데 카메라 포지션이 지금 안먹힌다 ㅠㅠㅠㅠ 이 안에서는 안먹히고
+        //activity_tour_view.xml에서 카메라 포지션 썻을떄는 먹힘. 그러나 캠퍼스에 따라 다르게 카메라를 보여주려면
+        //어떻게 해야할지 아직 해결책을 모르겠음
+        LatLng location = new LatLng(37.487936, 126.825071);
+        CameraPosition cameraPosition = new CameraPosition(location, 17);
+        naverMap.setCameraPosition(cameraPosition);
+        // 카메라 영역 제한...이것도 안먹힘. 왜지?
+        LatLng northWest = new LatLng(37.546836, 126.962978);   //서북단
+        LatLng southEast = new LatLng(37.543722, 126.966657);   //동남단
+        naverMap.setExtent(new LatLngBounds(northWest, southEast));
+
     }
 
 
@@ -58,6 +110,10 @@ public class Tour_View extends Activity {
     protected void onStart() {
         super.onStart();
         mapView.onStart();
+        LocationManager locationManager = (LocationManager)this.getSystemService(LOCATION_SERVICE);
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            gpsCheck();
+        }
     }
 
     @Override
